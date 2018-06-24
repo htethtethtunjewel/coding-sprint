e9484c11205c763d83661317da5524fdb7209488
htethtethtunjewel <htethtethtunhtet@gmail.com>
2018-06-24 21:57:04 +0630

    IS-A ,HAS-A object class

--- ex42.py
+++ ex42.py
@@ -11,8 +11,8 @@ class Person(object):
        self.name=name
        self.pet=None
 class Employee(Person):
-    def __init__(self,name,salary):
-       super(Employee,self).__init__(name)
+    def __init__(self, iname,salary):
+       super(Employee, self).__init__(name)
        self.salary =salary
 class Fish(object):
     pass
